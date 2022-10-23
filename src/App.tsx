42f2456d5083f90f0281d143b4cb27202f3faeae
matthewjljackson <matthewjljackson@gmail.com>
2022-10-23 17:41:08 +0100

    feat: use useSWR to fetch breweries

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+import axios, { AxiosResponse } from 'axios';
+import { Brewery, BrewerySchema } from './utils';
+
 function App() {
-  return <h1 className='text-3xl text-red-500 font-bold underline'>Hello world!</h1>;
+  const [endPoint, setEndPoint] = useState('');
+  const { data, error } = useSWR<AxiosResponse<unknown>>(BASE_URL + endPoint, axios);
+  const [breweries, setBreweries] = useState<Brewery[]>([]);
+
+  useEffect(() => {
+    if (data && Array.isArray(data.data)) {
+      const latestBreweries: Brewery[] = [];
+
+      data.data.forEach((brewery) => {
+        const parsed = BrewerySchema.safeParse(brewery);
+        if (parsed.success) {
+          latestBreweries.push(parsed.data);
+        }
+      });
+
+      setBreweries((prev) => {
+        return [...prev, ...latestBreweries];
+      });
+    }
+  }, [data]);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  console.log({ breweries });
+  return <h1 className='text-3xl text-red-500 font-bold underline'>Brewery Finder</h1>;
 }
 
 export default App;
+
+const BASE_URL = 'https://api.openbrewerydb.org/breweries';
---
+++ src/utils/brewery.ts
@@ -0,0 +1,10 @@
+import * as z from 'zod';
+
+export const BrewerySchema = z.object({
+  name: z.string().min(1),
+  longitude: z.string().min(1),
+  latitude: z.string().min(1),
+  city: z.string().min(1),
+});
+
+export type Brewery = z.infer<typeof BrewerySchema>;
